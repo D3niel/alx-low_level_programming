c9001779686596e0b22d2ef6387edd50187bcd07
root <kamwi24.dk@gmail.com>
2021-08-30 12:16:49 -0700

    "commit"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
 *
 * print using printf
 * 
-* return 0
+* Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
