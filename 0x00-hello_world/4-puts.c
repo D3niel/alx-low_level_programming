ed8deca2f4a94cdeb6d320d3f232ce7ca097e0c0
root <kamwi24.dk@gmail.com>
2021-08-30 02:48:40 -0700

    "commit"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
-/*using puts to print*/
-int main()
+
+/**
+ *
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
 {
 
-	   puts("Programming is like building a multilingual puzzle\n.");
+	puts("Programming is like building a multilingual puzzle\n.");
 		       return 0;
 }
