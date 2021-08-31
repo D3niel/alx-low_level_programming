5b6723372fc6b9213f148de2bfd21883c1c3d8b5
root <kamwi24.dk@gmail.com>
2021-08-31 12:48:30 -0700

    "commit"

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+*main - check the code for ALX School students.
+*
+* using printf to print text
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
