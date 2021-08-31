dbe158893f97535e24c68c5f8f679a284d2b5b1b
root <kamwi24.dk@gmail.com>
2021-08-31 11:47:39 -0700

    "commit"

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - check the code for ALX School students.
+*
+* using printf to print text
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return (0);
+}
