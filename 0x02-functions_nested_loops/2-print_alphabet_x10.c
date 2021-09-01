13d065905226042309825145abae588ddc97814b
root <kamwi24.dk@gmail.com>
2021-09-01 08:12:12 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#include <stdbool.h>
+
+/**
+* print_alphabet – function that prints lowercase alphabet
+*/
+
+void print_alphabet(void)
+
+{
+	int i = 97;
+	bool a = true;
+
+	while (a)
+	{
+	_putchar (i);
+
+		if (i == 122)
+		{
+			a = false;
+		}
+	i++;
+	}
+		for (i = 0; i < 11; i++);
+	{
+
+		_putchar ('\n');
+	}
+
+}
