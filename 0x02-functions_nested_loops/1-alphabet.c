30bf5adbc920c9c46f66227002805a775b53f5c9
root <kamwi24.dk@gmail.com>
2021-09-01 05:01:58 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+#include <stdbool.h>
+
+/**
+ *  print_alphabet – function that prints lowercase alphabet
+ *
+ */
+
+void print_alphabet (void)
+
+{
+	int i = 97;
+	bool a = true;
+
+	while (a)
+        {
+	_putchar (i);
+
+		if (i == 122)
+		{
+			a = false;
+		}
+	i++;
+	{
+		_putchar ('\n');
+	}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,5 @@
-#include <unistd.h>
-#include <stdio.h>
-
-/**
-*main - check the code for ALX School students.
-*
-* using printf to print text
-*
-* Return: Always 0.
-*/
-
-int main(void)
-{
-	printf("Holberton\n");
-	return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar (char c);
+void print_alphabet (void);
+#endif
