bd7fec2e60d3000baa9bc0b213732373fd97c5c6
root <kamwi24.dk@gmail.com>
2021-09-20 00:39:49 -0700

    print all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,7 +25,7 @@ int main(void)
 
 			putchar(i);
 			putchar(j);
-			
+
 			if (i == 56 && j == 57)
 			{
 				break;
@@ -38,5 +38,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return (0);
+	return (0)
 }
